5968f3758d516ef0ab231dcb7378b5f645cc15dc
jayden124 <jayden260100@gmail.com>
2020-04-16 20:49:00 +1200

    update

--- main/views.py
+++ main/views.py
@@ -12,7 +12,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep, strftime
 
-chromedriver_path = 'C:/Users/jayde/OneDrive/Documents/chromedriver_win32/chromedriver.exe' # Change this to your own chromedriver path!
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chromedriver_path = executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options 
 
 def clear(webElement):
     webElement.send_keys(Keys.CONTROL + "a");
